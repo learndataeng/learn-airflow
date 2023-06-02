7d4d05a3212b376077c201155eeaac99f249b269
learndataeng <135051097+learndataeng@users.noreply.github.com>
2023-06-01 19:45:19 -0700

    Create UpdateSymbol_v2.py

---
+++ dags/UpdateSymbol_v2.py
@@ -0,0 +1,81 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime
+from pandas import Timestamp
+
+import yfinance as yf
+import pandas as pd
+import logging
+
+
+def get_Redshift_connection(autocommit=True):
+    hook = PostgresHook(postgres_conn_id='redshift_dev_db')
+    conn = hook.get_conn()
+    conn.autocommit = autocommit
+    return conn.cursor()
+
+
+@task
+def get_historical_prices(symbol):
+    ticket = yf.Ticker(symbol)
+    data = ticket.history()
+    records = []
+
+    for index, row in data.iterrows():
+        date = index.strftime('%Y-%m-%d %H:%M:%S')
+
+        records.append([date, row["Open"], row["High"], row["Low"], row["Close"], row["Volume"]])
+
+    return records
+
+
+def _create_table(cur, schema, table):
+    cur.execute(f"DROP TABLE IF EXISTS {schema}.{table};")
+    cur.execute(f"""
+CREATE TABLE {schema}.{table} (
+    date date,
+    "open" float,
+    high float,
+    low float,
+    close float,
+    volume bigint
+);""")
+
+
+@task
+def load(schema, table, records):
+    logging.info("load started")
+    cur = get_Redshift_connection()
+    try:
+        cur.execute("BEGIN;")
+        # 원본 테이블 생성 - 테이블이 처음 한번 만들어질 때 필요한 코드
+        _create_table(cur, schema, table)
+        # 임시 테이블로 원본 테이블을 복사
+        cur.execute(f"CREATE TEMP TABLE t AS SELECT * FROM {schema}.{table};")
+        for r in records:
+            sql = f"INSERT INTO t VALUES ('{r[0]}', {r[1]}, {r[2]}, {r[3]}, {r[4]}, {r[5]});"
+            print(sql)
+            cur.execute(sql)
+
+        # 원본 테이블 생성
+        _create_table(cur, schema, table)
+        # 임시 테이블 내용을 원본 테이블로 복사
+        cur.execute(f"INSERT INTO {schema}.{table} SELECT DISTINCT * FROM t;")
+        cur.execute("COMMIT;")   # cur.execute("END;")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        cur.execute("ROLLBACK;")   
+    logging.info("load done")
+
+
+with DAG(
+    dag_id = 'UpdateSymbol_v2',
+    start_date = datetime(2023,5,30),
+    catchup=False,
+    tags=['API'],
+    schedule = '0 10 * * *'
+) as dag:
+
+    results = get_historical_prices("AAPL")
+    load("keeyong", "stock_info_v2", results)
