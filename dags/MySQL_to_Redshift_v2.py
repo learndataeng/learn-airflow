d2da297e9f058bc4d5d234d1160893350b30283c
learndataeng <135051097+learndataeng@users.noreply.github.com>
2023-06-04 19:16:21 -0700

    Update MySQL_to_Redshift_v2.py

--- dags/MySQL_to_Redshift_v2.py
+++ dags/MySQL_to_Redshift_v2.py
@@ -17,7 +17,7 @@ dag = DAG(
     start_date = datetime(2023,1,1), # 날짜가 미래인 경우 실행이 안됨
     schedule = '0 9 * * *',  # 적당히 조절
     max_active_runs = 1,
-    catchup = True,
+    catchup = False,
     default_args = {
         'retries': 1,
         'retry_delay': timedelta(minutes=3),
