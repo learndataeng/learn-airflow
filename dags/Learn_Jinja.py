cff18f1f83c650e7e6ddb60bd0286958f29f5dff
learndataeng <135051097+learndataeng@users.noreply.github.com>
2023-06-18 14:45:32 -0700

    Create Learn_Jinja.py

---
+++ dags/Learn_Jinja.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+# DAG 정의
+dag = DAG(
+    'Learn_Jinja',
+    schedule='0 0 * * *',  # 매일 실행
+    start_date=datetime(2023, 6, 1),
+)
+
+# BashOperator를 사용하여 템플릿 작업 정의
+task1 = BashOperator(
+    task_id='task1',
+    bash_command='echo "{{ ds }}"',
+    dag=dag
+)
+
+# 동적 매개변수가 있는 다른 템플릿 작업 정의
+task2 = BashOperator(
+    task_id='task2',
+    bash_command='echo "안녕하세요, {{ params.name }}!"',
+    params={'name': 'John'},  # 사용자 정의 가능한 매개변수
+    dag=dag
+)
+
+task3 = BashOperator(
+    task_id='task3',
+    bash_command="""echo "{{ dag }}, {{ task }}, {{ var.value.get('csv_url') }}" """,
+    dag=dag
+)
+
+task1 >> task2 >> task3
